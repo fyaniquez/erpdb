a2bfa98afcec720c68719fce83947a57a1b82685
Favio Yañiquez <siqeli@almacen.aymararu>
2020-07-16 22:09:26 -0400

    16 julio noch

--- app/controllers/facturas_controller.rb
+++ app/controllers/facturas_controller.rb
@@ -1,43 +1,71 @@
 class FacturasController < ApplicationController
   before_action :authenticate_user!
   before_action :set_factura, only: [:show, :edit, :update, :destroy]
+  # GET /facturas
+  # GET /facturas.json
+  def index
+    @facturas = Vendido.all
+  end
 
+  # GET /facturas/1
+  # GET /facturas/1.json
+  def show
+  end
   #GET /facturas/new
   def new
-    @factura = Venta.create(estado: "creando", usuario_id: current_user.id, almacen_id: 3, fecha: Date.today)
-    @vendido = Vendido.new(transaccion: @factura)
-    @productos = productos_lista
+    factura_general
   end
   # GET /facturas/1/edit
   def edit
-    @productos = Producto.all
+    @factura = Venta.new(estado: "creando", usuario_id: current_user.id, almacen_id: 3, fecha: Date.today)
+    #@vendido = Vendido.new(transaccion_id: @factura.id)
+    @vendido = @factura.detalles.new
+    @productos = productos_lista
   end
-  # PATCH/PUT /ventas/1
-  # PATCH/PUT /ventas/1.json
-  def update
+  # POST /facturas
+  def create
+    @factura = Venta.new(objeto: 1, glosa: "glosa de prueba", estado: "creando", usuario_id: current_user.id, almacen_id: 3, fecha: Date.today)
     respond_to do |format|
-      @vendido.create(venta_params[:producto_id])
-      if @venta.update(venta_params[:objeto])
-        format.html { redirect_to @venta, notice: 'Venta was successfully updated.' }
-        format.json { render :show, status: :ok, location: @venta }
+      if @factura.save
+        @vendido = Vendido.new(transaccion_id: @factura.id, producto_id: factura_params["producto_id"], precio: factura_params["precio"], cantidad: factura_params["cantidad"], total: factura_params["total"])
+        if @vendido.save
+          format.html { redirect_to edit_factura_path(@factura), notice: "todo ok" }
+          format.json { render :show, status: :created, location: @factura }
+        else
+          format.html { render :new }
+          format.json { render json: @vendido.errors, status: :unprocessable_entity }  
+        end
       else
-        format.html { render :edit }
-        format.json { render json: @venta.errors, status: :unprocessable_entity }
+        format.html { render :new }
+        format.json { render json: @factura.errors, status: :unprocessable_entity }
       end
-      if @venta.update(venta_params[:objeto])
-        format.html { redirect_to @venta, notice: 'Venta was successfully updated.' }
-        format.json { render :show, status: :ok, location: @venta }
+    end
+  end
+  def update    
+    respond_to do |format|
+      if @factura.update()
+        format.html { redirect_to edit_factura_path(@factura), notice: "todo ok" }
+        format.json { render :show, status: :created, location: @factura }
       else
-        format.html { render :edit }
-        format.json { render json: @venta.errors, status: :unprocessable_entity }
+        format.html { render :new }
+        format.json { render json: @factura.errors, status: :unprocessable_entity }
       end
     end
   end
   private
-  def productos_lista
-    Producto.order(:nombre).collect {|p| [p.nombre, {precio: p.precio}, p.id]}
-  end  
-  def factura_params
-    params.require(:venta).permit(:objeto, vendido: [:producto_id, :precio, :cantidad, :total])
-  end
+    def set_factura
+      @factura = Venta.find(params[:id])
+    end
+    def productos_lista
+      Producto.order(:nombre).collect {|p| [p.nombre, {precio: p.precio}, p.id]}
+    end  
+    def factura_params
+      params.require(:venta).permit(:objeto, :producto_id, :precio, :cantidad, :total)
+    end
+    def factura_general
+      @factura = Venta.new(estado: "creando", usuario_id: current_user.id, almacen_id: 3, fecha: Date.today)
+      #@vendido = Vendido.new(transaccion_id: @factura.id)
+      @vendido = @factura.detalles.new
+      @productos = productos_lista
+    end
 end
--- app/models/transacciones/venta.rb
+++ app/models/transacciones/venta.rb
@@ -1,2 +1,3 @@
 class Venta < Transaccion
+    
 end
